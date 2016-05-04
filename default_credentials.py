0ade45170e4edf0739024adc76173a734327adb0
Karthik Hariharan <karthikhariharan13@gmail.com>
2016-05-04 16:15:14 -0500

    Refactored to accomodate multiple API keys

--- default_credentials.py
+++ default_credentials.py
@@ -1,2 +1,12 @@
 
-access_token = ''
+citymapper_api_keys = ['']
+telegram_bot_key = ''
+
+
+def get_citymapper_api_key(seed):
+    index = seed % len(api_keys)
+    return api_keys[index]
+
+
+def get_telegram_bot_api_key():
+    return telegram_bot_key
