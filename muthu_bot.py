772ae933499a29e1d207e5ad88bc9f84e21b9269
Karthik Hariharan <karthikhariharan13@gmail.com>
2016-05-05 01:16:34 -0500

    Refactored and used the correct skeleton code

--- muthu_bot.py
+++ muthu_bot.py
@@ -1,53 +1,22 @@
-import telepot
-from telepot.delegate import per_from_id, create_open
+import asyncio
+import os
+import configparser
+import telepot.async
 from credentials import get_telegram_bot_api_key
+from muthu_tasks import execute_task
 
 
-class UserTracker(telepot.helper.UserHandler):
-    def __init__(self, seed_tuple, timeout):
-        super(UserTracker, self).__init__(seed_tuple, timeout)
-
-        # keep track of how many messages of each flavor
-        self._counts = {'chat': 0,
-                        'inline_query': 0,
-                        'chosen_inline_result': 0}
-
-        self._answerer = telepot.helper.Answerer(self.bot)
-
-    def on_message(self, msg_packet):
-        flavor = telepot.flavor(msg_packet)
-        self._counts[flavor] += 1
-
-        if flavor == 'chat':
-            print(msg_packet)
-            # command = msg_packet['text'].strip()
-
-
-
-def set_home(msg_packet):
-    
-    pass
-
-
-def set_work(msg_packet):
-    pass
-
-
-def set_dest(msg_packet):
-    pass
-
-
-command_mapper = {
-    '/home': set_home,
-    '/work': set_work,
-    '/new_dest': set_dest
-}
-
+@asyncio.coroutine
+def handle(msg_packet):
+    chat_id, response_text = execute_task(msg_packet)
+    yield from bot.sendMessage(chat_id, response_text)
 
 
 telegram_bot_api_key = get_telegram_bot_api_key()
 
-bot = telepot.DelegatorBot(telegram_bot_api_key, [
-    (per_from_id(), create_open(UserTracker, timeout=20)),
-])
-bot.message_loop(run_forever=True)
+bot = telepot.async.Bot(telegram_bot_api_key)
+loop = asyncio.get_event_loop()
+
+loop.create_task(bot.message_loop(handle))
+
+loop.run_forever()
