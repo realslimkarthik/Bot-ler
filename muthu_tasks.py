a90ffcdaa13f4743dc859e947e0ec007b87a6f36
Karthik Hariharan <karthikhariharan13@gmail.com>
2016-05-05 20:59:55 -0500

    Bugfixes and Refactoring

--- muthu_tasks.py
+++ muthu_tasks.py
@@ -3,80 +3,78 @@ import telepot
 
 
 def execute_task(msg_packet):
+    response_text = ''
     content_type, chat_type, chat_id = telepot.glance(msg_packet)
     flavor = telepot.flavor(msg_packet)
-    response_text = 'hello'
-
     if content_type == 'text':
         message_packet_fragments = parse_message_packet(msg_packet)
-        chat_id = message_packet_fragments['chat_id']
-        cmd_args = message_packet_fragments['arguments']
-        text = message_packet_fragments['text']
-        commands = message_packet_fragments['commands']
+        cmd_args = message_packet_fragments.get('arguments')
+        text = message_packet_fragments.get('text')
+        commands = message_packet_fragments.get('commands')
         command = commands[0]
         command_callback = command_mapper.get(command)
         if command_callback is not None:
-            response_text = command_callback(cmd_args, chat_id)
+            response_text = command_callback(command_arguments=cmd_args, chat_id=chat_id)
 
     return chat_id, response_text
 
 
-def send_help_msg(command_arguments, chat_id):
-    help_msg = ''
-    if command_arguments is None:
-        help_msg = '''
-            Hello
-            My name is Muthu. I am inspired by the character who has my same name in the 1995 hit Tamil film Muthu \
+def generate_help_msg(**kwargs):
+    help_msg = dedent('''\
+            Vanakam. My name is Muthu. I am inspired by the character who has my same name in the 1995 hit Tamil film Muthu \
             https://en.wikipedia.org/wiki/Muthu_(1995_film)
             I can help you with a lot of things. I've described them all below
-
-        '''
-        for command, command_text in help_msgs.items():
-            help_msg += command + command_text + '\n'
-    elif command_arguments:
-        for command_argument in command_arguments:
-            command_argument_text = help_msgs.get(command_argument)
-            help_msg += command_argument_text
-    help_msg = dedent(help_msg)
+        '''.strip())
+    for command_desc_text in help_msgs:
+        help_msg += '\n' + command_desc_text
     return help_msg
 
 
-def set_home(msg_packet):
+def set_home(**kwargs):
     
     pass
 
 
-def set_work(command_arguments, chat_id):
+def set_work(**kwargs):
     return 'hello'
 
 
-def create_new_trip(msg_packet):
+def create_new_trip(**kwargs):
     pass
 
 
 def parse_message_packet(message_packet):
-    msg_text = message_packet['text']
-    commands_positions = [{'offset': entity['offset'], 'length':entity['length']} for entity in message_packet['entities'] \
-                if entity['type'] == 'bot_command']
-    commands = [msg_text[cmd_pos['offset']:cmd_pos['offset'] + cmd_pos['length']] for cmd_pos in commands_positions]
-    arguments = ' '.join(msg_text.split()[1:])
-    if message_packet.get('from'):
-        chat_id = message_packet['from'].get('chat_id')
+    parsed_msg = {}
+    msg_text = message_packet.get('text')
+    commands = None
+
+    if message_packet.get('entities') and msg_text is not None:
+        parsed_msg['text'] = msg_text
+        commands_positions = [{'offset': entity['offset'], 'length':entity['length']} for entity in message_packet['entities'] \
+                    if entity['type'] == 'bot_command']
+        commands = [msg_text[cmd_pos['offset']:cmd_pos['offset'] + cmd_pos['length']] for cmd_pos in commands_positions]
     
-    return {'text': msg_text, 'chat_id': chat_id, 'arguments': arguments, 'commands':commands}
+    if commands is not None:
+        parsed_msg['commands'] = commands
+        arguments = ' '.join(msg_text.split()[1:])
+        if arguments == '':
+            arguments = None
+        else:
+            parsed_msg['arguments'] = arguments
+
+    return parsed_msg
 
 
 command_mapper = {
-    '/start': send_help_msg ,
-    '/help': send_help_msg,
+    '/start': generate_help_msg ,
+    '/help': generate_help_msg,
     '/home': set_home,
     '/work': set_work,
     '/new_trip': create_new_trip
 }
 
-help_msgs = {
-    '/home': ' - Set your home address for transit based tasks',
-    '/work': '- Set your work address for transit based tasks',
-    '/new_trip': '- Create a new trip to be prompted on when to leave and to try to get the best form of public transport',
-    '/help_with_cmd': '- Repeat this message. You can also Type /help_with_cmd /<command> to get details about a specific command'
-}
+help_msgs = [
+    '/home - set your home address',
+    '/new_trip - create a new trip to be prompted on when to leave',
+    '/work - set your work address'
+]
