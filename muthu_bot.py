dc5d3cc1eeba2725587faebc4d0bcf1b6118f74f
Karthik Hariharan <karthikhariharan13@gmail.com>
2016-05-05 20:59:12 -0500

    Checking for empty response_text

--- muthu_bot.py
+++ muthu_bot.py
@@ -9,7 +9,8 @@ from muthu_tasks import execute_task
 @asyncio.coroutine
 def handle(msg_packet):
     chat_id, response_text = execute_task(msg_packet)
-    yield from bot.sendMessage(chat_id, response_text)
+    if response_text != '':
+        yield from bot.sendMessage(chat_id, response_text)
 
 
 telegram_bot_api_key = get_telegram_bot_api_key()
